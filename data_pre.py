286da7d4496c9d2ad697f01dd56f9f7efad42f54
Rahul Pathak <31792379+iamrahul29@users.noreply.github.com>
2019-06-18 13:55:17 +0530

    Add files via upload

---
+++ data_pre.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('dataset/Data_Pre.csv')
+X = dataset.iloc[:, 0:3].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.impute import SimpleImputer
+sim = SimpleImputer()
+sim.fit(X[:, 0:2])
+X[:, 0:2] = sim.transform(X[:, 0:2])
+
+from sklearn.preprocessing import LabelEncoder
+lab = LabelEncoder()
+X[:, 2] = lab.fit_transform(X[:, 2])
+lab.classes_
+y = lab.fit_transform(y)
+
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder(categorical_features = [2])
+X = one.fit_transform(X)
+X = X.toarray()
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+
+
+
+
+
+
+
+
+
